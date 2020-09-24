65459c7f8da93663bb44350ae878e0b4bab1e104
Vetrivel-light <69761548+Vetrivel-light@users.noreply.github.com>
2020-09-24 03:58:44 -0700

    Update answer1.js

--- day3_Assignment/answer1.js
+++ day3_Assignment/answer1.js
@@ -1,15 +1,14 @@
 
-        const img1=document.getElementById('count');
+        const img1=document.getElementById("image");
         function third(){
-            var newImg=
-            "thor.jpeg"
+            var newImg="thor.jpeg";
             img1.src = newImg;
         }
         function second(){
-            var newImg2="capAmerica.jpeg"
+            var newImg2="capAmerica.jpeg";
             img1.src = newImg2;
         }
         function first(){
-            var newImg3="ironman.jpg"
+            var newImg3="ironman.jpg";
             img1.src = newImg3;
         }
